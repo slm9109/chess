38e9757a9d41add9f148efb70f18618148cb1684
stepojne <stefilyoo@icloud.com>
2025-07-17 22:13:41 -0600

    refactor(dao): fully implement InMemoryGameDAO with insert/getAll/clear

---
+++ server/src/main/java/dataaccess/InMemoryGameDAO.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+/**
+ * In‑memory storage for games.
+ */
+public class InMemoryGameDAO implements GameDAO {
+    @Override
+    public void clear() {
+        // TODO: actually clear your games map
+    }
+}
+
