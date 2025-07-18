36b71a9662bf775d9780ad4d90a52db03cd03ba1
stepojne <stefilyoo@icloud.com>
2025-07-17 22:27:10 -0600

    feat(dao): extend UserDAO and AuthDAO for register endpoints

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.AuthData;
+import dataaccess.DataAccessException;
+import java.util.List;
+
 public interface AuthDAO {
+    void clear();
+    void insertAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String token) throws DataAccessException;
+    List<AuthData> getAllAuths();
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.UserData;
+import dataaccess.DataAccessException;
+import java.util.List;
+
 public interface UserDAO {
+    void clear();
+    void insertUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    List<UserData> getAllUsers();
 }
