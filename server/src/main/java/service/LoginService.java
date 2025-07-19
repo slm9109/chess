d9a84101fc8db741466e963d703afe0ac361e786
stepojne <stefilyoo@icloud.com>
2025-07-18 22:13:54 -0600

    feat(service): implement LoginService.login()

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,10 @@
-package service;
-
-public class LoginService {
+public LoginResult login(LoginRequest req) throws DataAccessException {
+    if (req.username==null || req.password==null) throw new DataAccessException("Bad request");
+    var user = userDAO.getUser(req.username);
+    if (!user.getPassword().equals(req.password)) throw new DataAccessException("Unauthorized");
+    String token = UUID.randomUUID().toString();
+    authDAO.insertAuth(new AuthData(token, req.username));
+    return new LoginResult(req.username, token);
 }
+
+
